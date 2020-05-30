68dc323a2f1ee9c6588ef7055ccd8d9902ab45a0
pix106 <sbordenave@gmail.com>
2020-05-31 00:18:12 +0200

    ramdisk : initial ramdisk

--- device_Tab2A710F.mk
+++ device_Tab2A710F.mk
@@ -20,3 +20,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+# Ramdisk
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ramdisk/init.mt8127.rc:root/init.mt8127.rc \
+    $(LOCAL_PATH)/ramdisk/init.mt8127usb.rc:root/init.mt8127usb.rc \
+    $(LOCAL_PATH)/ramdisk/ueventd.mt8127.rc:root/ueventd.mt8127.rc \
+    $(LOCAL_PATH)/ramdisk/init.project.rc:root/init.project.rc \
+    $(LOCAL_PATH)/ramdisk/init.aee.rc:root/init.aee.rc \
+    $(LOCAL_PATH)/ramdisk/init.ssd.rc:root/init.ssd.rc \
+    $(LOCAL_PATH)/ramdisk/init.charging.rc:root/init.charging.rc \
+    $(LOCAL_PATH)/ramdisk/fstab:root/fstab \
+    $(LOCAL_PATH)/ramdisk/fstab.mt8127:root/fstab.mt8127 \
+    $(LOCAL_PATH)/ramdisk/enableswap.sh:root/enableswap.sh \
+    $(LOCAL_PATH)/ramdisk/auto_shutdown.sh:root/auto_shutdown.sh \
+    $(LOCAL_PATH)/ramdisk/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/ramdisk/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/ramdisk/meta_init.rc:root/meta_init.rc \
+    $(LOCAL_PATH)/ramdisk/meta_init.project.rc:root/meta_init.project.rc
+
