208d7e00472bf7c9871f62bbe72294a4b0a9465b
pix106 <sbordenave@gmail.com>
2020-05-30 23:45:39 +0200

    device : build su

--- device_Tab2A710F.mk
+++ device_Tab2A710F.mk
@@ -130,3 +130,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
 
+# Root
+PRODUCT_PACKAGES += \
+    su
+
